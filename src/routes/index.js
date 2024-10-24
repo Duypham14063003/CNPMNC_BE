4437cc2be34d3b4cfd7f099b4e3c6cd2407debb3
ChiNg22nd04 <dedonai99@gmail.com>
2024-10-24 12:12:00 +0700

    done role quantri + update router

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,16 @@ const authRoute = require("./auth.routes");
 const adminRoute = require("./admin.routes");
 const doctorRouter = require("./doctor.routes");
 const router = express.Router();
+const departmentRoute = require("./department.routes");
 const receptionistRoute = require("./receptionist.routes");
+const itSupportRoute = require("./itSupport.routes");
 
 router.use("/", authRoute);
 router.use("/user", userRoute);
 router.use("/admin", adminRoute);
 router.use("/doctor", doctorRouter);
+router.use("/department", departmentRoute);
 router.use("/receptionist", receptionistRoute);
+router.use("/itSupport", itSupportRoute);
 
 module.exports = router;
--- src/routes/routes/doctor.routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const doctorController = require("../controllers/doctor.controller");
-router.get("/listdoctor/:departmentId", doctorController.listDoctors); 
-module.exports = router;
